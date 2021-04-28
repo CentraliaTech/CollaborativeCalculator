3eb6092b0d3721d80c5b5507f22a47d017e54fa7
Mitch Partee <mitch.partee@student.centralia.edu>
2021-04-28 08:22:46 -0700

    Added Multiply and Divide Methods

--- Calculator.cs
+++ Calculator.cs
@@ -8,5 +8,13 @@ namespace CollaborativeCalculator
 {
     class Calculator
     {
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
     }
 }
